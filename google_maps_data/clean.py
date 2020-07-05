c4a85f52bdc47fd4c75c6ef7ff17d3e239a8b152
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-07-05 09:43:38 -0500

    removed the duplicates and stored them in the no_dup folder

---
+++ google_maps_data/clean.py
@@ -0,0 +1,11 @@
+import pandas as pd
+csv_list=['bakery.csv','home_goods_store.csv','supermarket.csv',
+'bank.csv','convenience_store.csv','liquor_store.csv',
+'bar.csv','department_store.csv','restaurant.csv',
+'cafe.csv','drugstore.csv', 'store.csv'
+]
+for i in range(12):
+    df = pd.read_csv(csv_list[i])
+    df.drop(inplace = True,columns="Unnamed: 0")
+    df.drop_duplicates(inplace = True,ignore_index=True)
+    df.to_csv("../no_dup/" + csv_list[i])
