bb3ef476d0815fab577f0c096fba253bf321eb16
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-07-05 10:12:21 -0500

    Created a master csv that combines the csvs

--- google_maps_data/clean.py
+++ google_maps_data/clean.py
@@ -7,5 +7,5 @@ csv_list=['bakery.csv','home_goods_store.csv','supermarket.csv',
 for i in range(12):
     df = pd.read_csv(csv_list[i])
     df.drop(inplace = True,columns="Unnamed: 0")
-    df.drop_duplicates(inplace = True,ignore_index=True)
+    df.drop_duplicates(inplace = True, ignore_index=True)
     df.to_csv("../no_dup/" + csv_list[i])
---
+++ no_dup/master_maker.py
@@ -0,0 +1,22 @@
+import pandas as pd
+csv_list=['bakery.csv','home_goods_store.csv','supermarket.csv',
+'convenience_store.csv','liquor_store.csv',
+'department_store.csv','restaurant.csv',
+'cafe.csv','drugstore.csv', 'store.csv'
+]
+names = []
+addresses = []
+lats = []
+lngs = []
+master_df = pd.DataFrame({
+    "Name": names,
+    "Address": addresses,
+    "Latitude": lats,
+    "Longitude": lngs,
+})
+for i in range(10):
+    df = pd.read_csv(csv_list[i])
+    df.drop(inplace = True,columns="Unnamed: 0")
+    master_df = master_df.append(df,ignore_index=True)
+
+master_df.to_csv("../Resources/master.csv")
