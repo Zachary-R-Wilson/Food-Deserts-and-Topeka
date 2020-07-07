50a9cbe2900b2e8157f0f34f75a2d2e0fc7a913d
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-07-06 19:35:32 -0500

    Added a table name row to the master csv

--- no_dup/master_maker.py
+++ no_dup/master_maker.py
@@ -4,19 +4,27 @@ csv_list=['bakery.csv','home_goods_store.csv','supermarket.csv',
 'department_store.csv','restaurant.csv',
 'cafe.csv','drugstore.csv', 'store.csv'
 ]
+table_list=['bakery','home_goods_store','supermarket',
+'convenience_store','liquor_store',
+'department_store','restaurant',
+'cafe','drugstore', 'store'
+]
 names = []
 addresses = []
 lats = []
 lngs = []
+csv_name = []
 master_df = pd.DataFrame({
     "Name": names,
     "Address": addresses,
     "Latitude": lats,
     "Longitude": lngs,
+    "Table Name":csv_name
 })
 for i in range(10):
     df = pd.read_csv(csv_list[i])
     df.drop(inplace = True,columns="Unnamed: 0")
+    df["Table Name"] = table_list[i]
     master_df = master_df.append(df,ignore_index=True)
 
 master_df.drop_duplicates(inplace = True, ignore_index=True)
