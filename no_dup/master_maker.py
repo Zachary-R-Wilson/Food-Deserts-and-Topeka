29c5abb9433cf9c3e38d31f751012e07ec6845b4
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-07-07 06:31:15 -0500

    Added tracts to the master document

--- no_dup/master_maker.py
+++ no_dup/master_maker.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import censusgeocode as cg
+import time
+
 csv_list=['bakery.csv','home_goods_store.csv','supermarket.csv',
 'convenience_store.csv','liquor_store.csv',
 'department_store.csv','restaurant.csv',
@@ -14,17 +17,31 @@ addresses = []
 lats = []
 lngs = []
 csv_name = []
+tract = []
 master_df = pd.DataFrame({
     "Name": names,
     "Address": addresses,
     "Latitude": lats,
     "Longitude": lngs,
-    "Table Name":csv_name
+    "Table Name":csv_name,
+    "Tract": tract
 })
 for i in range(10):
     df = pd.read_csv(csv_list[i])
     df.drop(inplace = True,columns="Unnamed: 0")
     df["Table Name"] = table_list[i]
+    
+    result_list = []
+    for j in range(df.shape[0]):
+        try:
+            result = cg.coordinates(x=df["Longitude"][j], y=df["Latitude"][j])
+            spl_re = result['Census Tracts'][0]['NAME']
+            spl_re = spl_re.split(' ')
+            result_list.append(spl_re[2])
+        except:
+            result_list.append("")
+        time.sleep(2)
+    df["Tract"] = result_list
     master_df = master_df.append(df,ignore_index=True)
 
 master_df.drop_duplicates(inplace = True, ignore_index=True)
